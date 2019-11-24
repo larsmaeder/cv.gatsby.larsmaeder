f9e93f9df010e6ffddc67e718de69d4ead6d2acf
Lars Mäder <mail@larsmaeder.ch>
2019-11-24 14:07:09 +0100

    MOD: change from node to custom component

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,27 +1,25 @@
 import React from "react"
-import VisibilitySensor from "react-visibility-sensor"
+import VisibilitySensor from "../react-visibility-sensor-once"
 
 import Style from "./header.module.scss"
 
-function onChange(isVisible) {
-  console.log("Element is now %s", isVisible ? "visible" : "hidden")
-}
-
 const HeaderSection = ({ breadcrumbTitle }) => (
-  <VisibilitySensor partialVisibility onChange={onChange}>
-    <header className="animUp animUp--active">
-      <section className={Style.headerWrapper}>
-        <div className="gridContainer">
-          <div className="grid-noBottom">
-            <div className="col">
-              <p className={Style.breadcrumb}>
-                Lars Mäder<span>{breadcrumbTitle}</span>
-              </p>
+  <VisibilitySensor partialVisibility once>
+    {({ isVisible }) => (
+      <header className={"animUp" + (isVisible ? " in" : " off")}>
+        <section className={Style.headerWrapper}>
+          <div className="gridContainer">
+            <div className="grid-noBottom">
+              <div className="col">
+                <p className={Style.breadcrumb}>
+                  Lars Mäder<span>{breadcrumbTitle}</span>
+                </p>
+              </div>
             </div>
           </div>
-        </div>
-      </section>
-    </header>
+        </section>
+      </header>
+    )}
   </VisibilitySensor>
 )
 
