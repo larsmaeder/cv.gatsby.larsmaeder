e739492d8269374f6d45eb42da79575c9e2823ce
Lars Mäder <mail@larsmaeder.ch>
2019-12-01 11:13:14 +0100

    MOD: structure, styles

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,13 +3,13 @@ import VisibilitySensor from "../react-visibility-sensor-once"
 
 import Style from "./header.module.scss"
 
-const HeaderSection = ({ breadcrumbTitle }) => (
+const Header = ({ breadcrumbTitle }) => (
   <VisibilitySensor partialVisibility once>
     {({ isVisible }) => (
       <header className={"animUp" + (isVisible ? " in" : " off")}>
         <section className={Style.headerWrapper}>
           <div className="gridContainer">
-            <div className="grid-noBottom">
+            <div className="grid">
               <div className="col">
                 <p className={Style.breadcrumb}>
                   Lars Mäder<span>{breadcrumbTitle}</span>
@@ -23,4 +23,4 @@ const HeaderSection = ({ breadcrumbTitle }) => (
   </VisibilitySensor>
 )
 
-export default HeaderSection
+export default Header
--- src/components/intro/intro.js
+++ src/components/intro/intro.js
@@ -1,16 +1,17 @@
 import React from "react"
 import AnchorLink from "react-anchor-link-smooth-scroll"
+import { Link } from "gatsby"
 
 import Style from "./intro.module.scss"
 
-const IntroSection = () => (
+const Intro = () => (
   <section className={Style.introWrapper}>
     <div className="gridContainer">
       <div className="grid-3">
         <div className="col-12_lg-8">
           <h1>
-            Hi! Ich bin Lars. &nbsp;
-            <span role="img" aria-label="winkende Hand">
+            Hi! Ich bin Lars.&nbsp;
+            <span role="img" aria-label="Emoji winkende Hand">
               👋
             </span>
           </h1>
@@ -20,13 +21,14 @@ const IntroSection = () => (
             erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
             et ea rebum.
           </p>
-          <AnchorLink offset="100" href="#cv-start">
-            Gehe zu meinem CV
+          <AnchorLink className={Style.primaryButton + " " + Style.button} offset="100" href="#cv-start">
+            CV ansehen
           </AnchorLink>
+          <Link className={Style.secondaryButton + " " + Style.button} to="/">CV herunterladen</Link>
         </div>
       </div>
     </div>
   </section>
 )
 
-export default IntroSection
+export default Intro
